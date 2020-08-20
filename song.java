a329d98613aae5b286468c4bbaf32ffd7ffbedea
ShaneP2 <21shane.pearson@boiseschools.net>
2020-08-20 15:32:58 +0000

    everything

---
+++ song.java
@@ -0,0 +1,78 @@
+
+//Shane Pearson
+//AP Computer Science P.2
+//8-20-2020
+
+public class song{
+
+
+  public static void main(String[] args) {
+    System.out.println("Run away with me");
+    System.out.println("Lost souls and reverie");
+    System.out.println("");
+    System.out.println("Running wild and running free");
+    System.out.println("Two kids, you and me");
+    System.out.println("");
+    System.out.println("And I say");
+    System.out.println("Hey, hey hey hey");
+    System.out.println("Living like we're renegades");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("__________________________s$______________s");
+    System.out.println("_________________________.s$$_____________s$");
+    System.out.println("________________________s$$$’____________s$$");
+    System.out.println("______________________.s$$$³´_______,___s$$³");
+    System.out.println("_____________________s$$$$³______.s$’___$$³");
+    System.out.println("________________,____$$$$$.______s$³____³$");
+    System.out.println("________________$___$$$$$$s_____s$³_____³,");
+    System.out.println("_______________s$___‘³$$$$$$s___$$$");
+    System.out.println("_______________$$____³$$$$$$s.__³$$s");
+    System.out.println("_______________³$.____³$$$$$$$s_.s$$$____s´");
+    System.out.println("_______________`$$.____³$$$$$$$_$$$$___s³");
+    System.out.println("________________³$$s____³$$$$$$s$$$³__s$’");
+    System.out.println("_________________³$$s____$$$$$s$$$$’__s$$");
+    System.out.println("_____________`s.__$$$$___s$$$$$$$$³_.s$$³__s");
+    System.out.println("______________$$_s$$$$..s$$$$$$$$$$$$$$³__s$");
+    System.out.println("______________s$.s$$$$s$$$$$$$$$$$$$$$$_s$$");
+    System.out.println("_____________s$$$$$$$$$$$$$$$$$$$$$$$$$$$³");
+    System.out.println("____________s$$$ssss$$$$$$$$$$$$$ssss$$$$$´");
+    System.out.println("___________$$s§§§§§§§§§s$$$$$$s§§§§§§§§s$,");
+    System.out.println("___________³§§§§§§§§§§§§§s$s§§§§§§§§§§§§s");
+    System.out.println("___________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§");
+    System.out.println("___________³§§§§§§§§§§§§§§§§§§§§§§§§§§§§§");
+    System.out.println("____________³§§§§§§§§§§§§§§§§§§§§§§§§§§§³");
+    System.out.println("_____________³§§§§§§§§§§§§§§§§§§§§§§§§§³");
+    System.out.println("______________³§§§§§§§§§§§§§§§§§§§§§§§³");
+    System.out.println("________________³§§§§§§§§§§§§§§§§§§§³");
+    System.out.println("____________________³§§§§§§§§§§§³");
+    System.out.println("_______________________³§§§§§³");
+    System.out.println("_________________________³§³");
+  }
+}
+// use java song in -> to run the thing
+
+/*
+1: Run away with me
+2: Lost souls and reverie
+3: Running wild and running free
+4: Two kids, you and me
+
+5: And I say
+6: Hey, hey hey hey
+7: Living like we're renegades
+
+
+
+
+
+
+
+
+
+
+
+
+*/
